52cc7d716e054d7ee9a9ad6652a8707b315bf247
William Lacerda <williamlacerda959@gmail.com>
2024-02-28 23:51:17 -0300

    .

---
+++ aula11/exercicio008.js
@@ -0,0 +1,7 @@
+var velocidade = 12
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if(velocidade > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, você foi multado!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
+//Condição simples
---
+++ aula11/exercicio009.js
@@ -0,0 +1,7 @@
+var país = 'Espanha'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') { //pode se usar também != (diferente) no lugar de == (igual)
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
